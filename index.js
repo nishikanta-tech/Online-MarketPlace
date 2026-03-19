102f4fc5db320ed8f8e7d3e10b14a829eb9d693a
Nishikanta Jena <nishikanta394@gmail.com>
2026-03-19 11:44:49 +0530

    Update CORS settings to allow multiple origins

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use('/Products/images',express.static(path.join(__dirname,'Products/images')));
 
+
+
 app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    Credentials: true
-}))
+  origin: [
+    'http://localhost:5173',
+    'https://online-marketplace-project.netlify.app'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 mongoose.connect('mongodb+srv://nishikanta394:nishi394@cluster0.krwzypv.mongodb.net/OMP')
 .then(()=>{
     console.log('Connected to MongoDB');
